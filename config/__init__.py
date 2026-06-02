435f854191f75c6538adc41b15a6b0e2009db684
donsidelbe <donasiidelapanbelas@gmail.com>
2026-06-03 05:24:49 +0800

    chore(config): tambah boilerplate config/settings.py scaffolding

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+"""
+Nama Modul: config/__init__.py
+Deskripsi: Package inisialisasi modul konfigurasi runtime AbuCom.
+           Expose fungsi dan variabel settings.
+Author: GPT-OSS 120B (STK-014)
+Tanggal: 2026-06-03
+"""
+
+from config.settings import load_settings
